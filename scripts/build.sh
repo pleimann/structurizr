a6bf0dad58d63eeba71d6c6ead5e34022a2408f2
Mike Pleimann <pleimannm@objectcomputing.com>
2025-07-13 09:06:07 -0500

    Update scripts for more flexibility

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(dirname -- "$(readlink -f "${BASH_SOURCE}")")
+PROJECT_DIR=$SCRIPT_DIR/../
 
-env GOOS=darwin GOARCH=arm64 go build -ldflags "-s" -o strender-darwin-arm64 $SCRIPT_DIR/../
+env GOOS=darwin GOARCH=arm64 go build -ldflags "-s" -o bin/strender-darwin-arm64 $PROJECT_DIR
 
-env GOOS=darwin GOARCH=arm64 go build -ldflags "-s" -o strender-darwin-amd64 $SCRIPT_DIR/../
+env GOOS=darwin GOARCH=arm64 go build -ldflags "-s" -o bin/strender-darwin-amd64 $PROJECT_DIR
 
-env GOOS=linux GOARCH=amd64 go build -ldflags "-s" -o strender-linux-amd64 $SCRIPT_DIR/../
+env GOOS=linux GOARCH=amd64 go build -ldflags "-s" -o bin/strender-linux-amd64 $PROJECT_DIR
 
-env GOOS=linux GOARCH=amd64 go build -ldflags "-s" -o strender-linux-arm64 $SCRIPT_DIR/../
+env GOOS=linux GOARCH=amd64 go build -ldflags "-s" -o bin/strender-linux-arm64 $PROJECT_DIR
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(dirname -- "$(readlink -f "${BASH_SOURCE}")")
+PROJECT_DIR=$SCRIPT_DIR/../
 
 $SCRIPT_DIR/build.sh
 
-cp $SCRIPT_DIR/../strender-darwin-arm64 ~/bin/strender
+ARCH=$(arch)
+PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
+
+cp $PROJECT_DIR/bin/strender-$PLATFORM-$ARCH ~/bin/strender
